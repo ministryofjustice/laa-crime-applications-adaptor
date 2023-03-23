2041abe1229c4082e55f464e1ffc2604e716d47f
Gangadhar.Nitta <gangadhar.nitta@digital.justice.gov.uk>
2023-03-23 16:33:47 +0000

    Initial setup

---
+++ crime-applications-adaptor/src/main/java/uk/gov/justice/laa/crime/applications/adaptor/CrimeApplicationsAdaptorApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.justice.laa.crime.applications.adaptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimeApplicationsAdaptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrimeApplicationsAdaptorApplication.class, args);
+	}
+
+}
---
+++ crime-applications-adaptor/src/test/java/uk/gov/justice/laa/crime/applications/adaptor/CrimeApplicationsAdaptorApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.justice.laa.crime.applications.adaptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimeApplicationsAdaptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
