7cd1d988d1d12ec0e1081318e544e4031b6903b2
Alex Stone <alex.stone@digital.justice.gov.uk>
2023-05-17 16:22:24 +0100

    LASB-1964 Move boiler plate ApiResponse annotations to common reusable annotation

--- crime-applications-adaptor/src/main/java/uk/gov/justice/laa/crime/applications/adaptor/controller/CrimeApplicationController.java
+++ crime-applications-adaptor/src/main/java/uk/gov/justice/laa/crime/applications/adaptor/controller/CrimeApplicationController.java
@@ -1,14 +1,10 @@
 package uk.gov.justice.laa.crime.applications.adaptor.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
-import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,31 +20,12 @@ import uk.gov.justice.laa.crime.applications.adaptor.service.EformStagingService
 @Tag(name = "Crime Application Adaptor", description = "Rest API to retrieve application details from crime apply datastore")
 public class CrimeApplicationController {
 
-    private CrimeApplicationService crimeApplicationService;
-
-    private EformStagingService eformStagingService;
+    private final CrimeApplicationService crimeApplicationService;
+    private final EformStagingService eformStagingService;
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @Operation(description = "Retrieve application details from crime apply datastore")
-    @ApiResponse(responseCode = "200",
-            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
-                    schema = @Schema(implementation = MaatApplication.class)
-            )
-    )
-
-    @ApiResponse(responseCode = "400",
-            description = "Bad request.",
-            content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
-                    schema = @Schema(implementation = ProblemDetail.class)
-            )
-    )
-
-    @ApiResponse(responseCode = "500",
-            description = "Server Error.",
-            content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
-                    schema = @Schema(implementation = ProblemDetail.class)
-            )
-    )
+    @StandardApiResponseCodes
     public MaatApplication getCrimeApplyData(@PathVariable Long id) {
         log.info("Get applicant details from Crime Apply datastore");
         eformStagingService.retrieveOrInsertDummyUsnRecord(id);
---
+++ crime-applications-adaptor/src/main/java/uk/gov/justice/laa/crime/applications/adaptor/controller/StandardApiResponseCodes.java
@@ -0,0 +1,34 @@
+package uk.gov.justice.laa.crime.applications.adaptor.controller;
+
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
+import uk.gov.justice.laa.crime.applications.adaptor.model.MaatApplication;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@ApiResponses(value = {
+        @ApiResponse(responseCode = "200",
+                content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                        schema = @Schema(implementation = MaatApplication.class))),
+
+        @ApiResponse(responseCode = "400",
+                description = "Bad request.",
+                content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+                        schema = @Schema(implementation = ProblemDetail.class))),
+
+        @ApiResponse(responseCode = "500",
+                description = "Server Error.",
+                content = @Content(mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+                        schema = @Schema(implementation = ProblemDetail.class)))
+})
+public @interface StandardApiResponseCodes {
+}
