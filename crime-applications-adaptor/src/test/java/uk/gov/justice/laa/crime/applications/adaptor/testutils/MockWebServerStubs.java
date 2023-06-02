cec2459f32bd91e3d7b435136094f965fbc3af79
Alex Stone <alex.stone@digital.justice.gov.uk>
2023-06-02 10:52:32 +0100

    Code improvements

--- crime-applications-adaptor/src/test/java/uk/gov/justice/laa/crime/applications/adaptor/testutils/MockWebServerStubs.java
+++ crime-applications-adaptor/src/test/java/uk/gov/justice/laa/crime/applications/adaptor/testutils/MockWebServerStubs.java
@@ -37,7 +37,7 @@ public class MockWebServerStubs {
         };
     }
 
-    private enum RequestPathResponseMapping {
+    enum RequestPathResponseMapping {
         EFORM_STAGING_WITH_NO_MAAT_REF("/initialise/6000308", "data/eformstaging/EformStagingResponse_WithNoMaatRef_6000308.json", HttpStatus.OK),
         EFORM_STAGING_WITH_MAAT_REF("/initialise/6000288", "data/eformstaging/EformStagingResponse_WithMaatRef_6000288.json", HttpStatus.OK),
         EFORM_STAGING_HUB_USER("/initialise/6000310", "data/eformstaging/EformStagingResponse_CreatedByHub_6000310.json", HttpStatus.OK),
@@ -58,11 +58,15 @@ public class MockWebServerStubs {
             this.httpResponseStatus = httpResponseStatus;
         }
 
-        public boolean matchesRequestPath(String requestPathParameter) {
+        boolean matchesRequestPath(String requestPathParameter) {
             return requestPath.equals(requestPathParameter);
         }
 
-        public MockResponse getMockResponse() {
+        String getRequestPath() {
+            return requestPath;
+        }
+
+        MockResponse getMockResponse() {
             MockResponse mockResponse = new MockResponse()
                     .addHeader("Content-Type", MediaType.APPLICATION_JSON)
                     .setResponseCode(httpResponseStatus.value());
