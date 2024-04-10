1019eee58a322c084105716a062e965d58cfb5d3
Michael Clancy <michael.clancy@digital.justice.gov.uk>
2024-04-10 10:30:29 +0100

    LASB-3184: Updated assertion to use JSONAssert.

--- crime-applications-adaptor/api-tests/src/test/java/uk/gov/justice/laa/crime/applications/adaptor/stepdefinition/APITestSteps.java
+++ crime-applications-adaptor/api-tests/src/test/java/uk/gov/justice/laa/crime/applications/adaptor/stepdefinition/APITestSteps.java
@@ -1,9 +1,5 @@
 package uk.gov.justice.laa.crime.applications.adaptor.stepdefinition;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-
-import io.cucumber.java.After;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -11,6 +7,9 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import java.io.File;
 import net.serenitybdd.annotations.Steps;
+import org.json.JSONException;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import uk.gov.justice.laa.crime.applications.adaptor.apispecification.CrimeApplicationsAdaptorAPI;
 import uk.gov.justice.laa.crime.applications.adaptor.apispecification.CrimeApplyMockAPI;
 
@@ -39,8 +38,10 @@ public class APITestSteps {
   }
 
   @Then("the returned response should match the contents of {string}")
-  public void theFollowingDataShouldBeReturned(String expectedResponseFile) {
-    JsonPath expectedJson = new JsonPath(new File(EXPECTED_RESPONSE_FILE_PATH_BASE + expectedResponseFile));
-    assertThat(caaResponse.body().jsonPath().getMap(""), equalTo(expectedJson.getMap("")));
+  public void theFollowingDataShouldBeReturned(String expectedResponseFile) throws JSONException {
+    JsonPath expectedJson = new JsonPath(
+        new File(EXPECTED_RESPONSE_FILE_PATH_BASE + expectedResponseFile));
+    JSONAssert.assertEquals(expectedJson.prettify(), caaResponse.body().asPrettyString(),
+        JSONCompareMode.LENIENT);
   }
 }
