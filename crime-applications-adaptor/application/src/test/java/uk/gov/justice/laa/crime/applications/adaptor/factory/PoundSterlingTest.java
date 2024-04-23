5c5b5f8bf640e4104a9f7dacf0bee0c0b8531c4c
Josh Hunt <30326121+JHunt16@users.noreply.github.com>
2024-04-23 10:09:18 +0100

    [LASB-3224] Add additional test cases for PoundSterling and a small refactor.

--- crime-applications-adaptor/application/src/test/java/uk/gov/justice/laa/crime/applications/adaptor/factory/PoundSterlingTest.java
+++ crime-applications-adaptor/application/src/test/java/uk/gov/justice/laa/crime/applications/adaptor/factory/PoundSterlingTest.java
@@ -14,12 +14,14 @@ public class PoundSterlingTest {
   void shouldSuccessfullyConvertPenniesToPounds(Number pennies, BigDecimal expectedValue) {
     BigDecimal actualValue = PoundSterling.ofPennies(pennies).toPounds();
 
-    assertEquals(actualValue, expectedValue);
+    assertEquals(expectedValue, actualValue);
   }
 
   private static Stream<Arguments> pennyValues() {
     return Stream.of(
         Arguments.of(0, new BigDecimal("0.00")),
+        Arguments.of(101, new BigDecimal("1.01")),
+        Arguments.of(199, new BigDecimal("1.99")),
         Arguments.of(12500, new BigDecimal("125.00")),
         Arguments.of(1000, new BigDecimal("10.00")),
         Arguments.of(13550, new BigDecimal("135.50")),
