d353905ae4538c7fc5f2bb56cce7f03219872bd2
Ross-Nation <139126932+Ross-Nation@users.noreply.github.com>
2024-08-27 13:59:20 +0100

    Mappers updated to remove unnecessary code

--- crime-applications-adaptor/application/src/main/java/uk/gov/justice/laa/crime/applications/adaptor/mapper/crimeapply/OtherIncomeMapper.java
+++ crime-applications-adaptor/application/src/main/java/uk/gov/justice/laa/crime/applications/adaptor/mapper/crimeapply/OtherIncomeMapper.java
@@ -12,14 +12,6 @@ import uk.gov.justice.laa.crime.model.common.crimeapplication.common.AssessmentD
 import uk.gov.justice.laa.crime.model.common.criminalapplicationsdatastore.general.IncomePayment;
 
 public class OtherIncomeMapper {
-  private static final String STUDENT_LOAN_GRANT = "Student grant or loan";
-  private static final String BOARD_FROM_FAMILY =
-      "Board from family members living with your client";
-  private static final String RENT = "Rent from a tenant";
-  private static final String FROM_FRIENDS_RELATIVES = "Money from friends or family";
-  private static final String FINANCIAL_SUPPORT_WITH_ACCESS =
-      "Financial support from someone who allows your client access to their assets or money";
-
   public List<AssessmentDetail> mapOtherIncome(List<IncomePayment> otherIncome) {
     List<AssessmentDetail> assessmentDetails = new ArrayList<>();
 
@@ -53,21 +45,6 @@ public class OtherIncomeMapper {
     for (IncomePayment other : otherIncome) {
       if (Objects.nonNull(other.getDetails())) {
         otherBenefitNotes.add(other.getDetails().toString());
-      }
-
-      String incomeType = other.getPaymentType().value();
-      OtherIncomeDetails otherIncomeDetail = OtherIncomeDetails.findByValue(incomeType);
-      String note;
-      switch (otherIncomeDetail) {
-        case STUDENT_LOAN_GRANT -> note = STUDENT_LOAN_GRANT;
-        case BOARD_FROM_FAMILY -> note = BOARD_FROM_FAMILY;
-        case RENT -> note = RENT;
-        case FROM_FRIENDS_RELATIVES -> note = FROM_FRIENDS_RELATIVES;
-        case FINANCIAL_SUPPORT_WITH_ACCESS -> note = FINANCIAL_SUPPORT_WITH_ACCESS;
-        default -> note = StringUtils.EMPTY;
-      }
-      if (!StringUtils.EMPTY.equals(note)) {
-        otherBenefitNotes.add(note);
       }
     }
     return String.join("\n", otherBenefitNotes);
