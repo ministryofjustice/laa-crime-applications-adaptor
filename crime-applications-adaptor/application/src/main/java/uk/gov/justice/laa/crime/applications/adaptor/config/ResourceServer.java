83dbcada00e91ffdaca6a384d1e4887bb16eea17
Ross-Nation <139126932+Ross-Nation@users.noreply.github.com>
2024-09-17 11:51:02 +0100

    Fixed security config oopsy

--- crime-applications-adaptor/application/src/main/java/uk/gov/justice/laa/crime/applications/adaptor/config/ResourceServer.java
+++ crime-applications-adaptor/application/src/main/java/uk/gov/justice/laa/crime/applications/adaptor/config/ResourceServer.java
@@ -2,8 +2,10 @@ package uk.gov.justice.laa.crime.applications.adaptor.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,7 +15,22 @@ public class ResourceServer {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-    http.authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().permitAll());
+    http.authorizeHttpRequests(
+            authorizeHttpRequests ->
+                authorizeHttpRequests
+                    .requestMatchers("/token/**")
+                    .permitAll()
+                    .requestMatchers("/swagger-ui/**")
+                    .permitAll()
+                    .requestMatchers("/api-docs/**")
+                    .permitAll()
+                    .requestMatchers("/actuator/**")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated())
+        .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .oauth2ResourceServer(
+            (oauth2ResourceServer) -> oauth2ResourceServer.jwt(Customizer.withDefaults()));
 
     return http.build();
   }
