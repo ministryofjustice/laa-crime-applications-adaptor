0f2f4616a686b54d406334e619b8e5334877ed15
Ross-Nation <139126932+Ross-Nation@users.noreply.github.com>
2024-09-17 11:33:47 +0100

    Updated Service Test to use actual mapper method

--- crime-applications-adaptor/application/src/test/java/uk/gov/justice/laa/crime/applications/adaptor/service/CrimeApplicationResultServiceTest.java
+++ crime-applications-adaptor/application/src/test/java/uk/gov/justice/laa/crime/applications/adaptor/service/CrimeApplicationResultServiceTest.java
@@ -66,22 +66,62 @@ class CrimeApplicationResultServiceTest {
             .meansInitStatus("COMPLETE")
             .meansFullResult("PASS")
             .meansFullStatus("COMPLETE")
+            .meansAssessorName("Phineas Flynn")
+            .dateMeansCreated(LocalDate.of(2024, 5, 6).atStartOfDay())
+            .iojResult("PASS")
+            .iojReason(null)
+            .iojAssessorName("Phineas Flynn")
+            .dateAppCreated(LocalDate.of(2024, 5, 5))
+            .caseId("TEST012345")
+            .caseType("INDICTABLE")
+            .passportResult(null)
+            .passportStatus(null)
+            .passportAssessorName(null)
+            .datePassportCreated(null)
+            .fundingDecision("GRANTED")
             .build();
 
     CrimeApplicationResult expected = new CrimeApplicationResult();
     expected.setMaatRef(123456);
     expected.setUsn(123455);
+    expected.setCaseId("TEST012345");
+    expected.setCaseType(CaseType.INDICTABLE);
+    expected.setIojResult("PASS");
+    expected.setIojReason(null);
+    expected.setIojAssessorName("Phineas Flynn");
+    expected.setAppCreatedDate(LocalDate.of(2024, 5, 5).atStartOfDay());
     expected.setMeansResult("PASS");
-
+    expected.setMeansAssessorName("Phineas Flynn");
+    expected.setDateMeansCreated(LocalDate.of(2024, 5, 6).atStartOfDay());
+    expected.setFundingDecision("GRANTED");
 
     when(maatCourtDataApiClient.retrieveCrimeApplicationResultsByRepId(123456))
         .thenReturn(repOrderState);
-    when(crimeApplicationResultMapper.map(repOrderState)).thenReturn(expected);
+    when(crimeApplicationResultMapper.map(repOrderState)).thenCallRealMethod();
 
     CrimeApplicationResult actual = crimeApplicationResultService.getCrimeApplicationResultByRepId(123456);
 
     verify(maatCourtDataApiClient, times(1)).retrieveCrimeApplicationResultsByRepId(123456);
     assertEquals(expected.getMaatRef(), actual.getMaatRef());
+    assertEquals(expected.getUsn(), actual.getUsn());
+    assertEquals(expected.getCaseId(), actual.getCaseId());
+    assertEquals(expected.getCaseType(), actual.getCaseType());
+    assertEquals(expected.getIojResult(), actual.getIojResult());
+    assertEquals(expected.getIojReason(), actual.getIojReason());
+    assertEquals(expected.getIojAssessorName(), actual.getIojAssessorName());
+    assertEquals(expected.getAppCreatedDate(), actual.getAppCreatedDate());
     assertEquals(expected.getMeansResult(), actual.getMeansResult());
+    assertEquals(expected.getMeansAssessorName(), actual.getMeansAssessorName());
+    assertEquals(expected.getDateMeansCreated(), actual.getDateMeansCreated());
+    assertEquals(expected.getFundingDecision(), actual.getFundingDecision());
+  }
+
+  @Test
+  void shouldThrowWebClientRequestException_givenNetworkUnreachableOnInvokingMaatCourtDataAPIByRepId() {
+    when(maatCourtDataApiClient.retrieveCrimeApplicationResultsByRepId(123456))
+        .thenThrow(WebClientRequestException.class);
+    assertThrows(
+        WebClientRequestException.class,
+        () -> crimeApplicationResultService.getCrimeApplicationResultByRepId(123456));
   }
 }
